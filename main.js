cf839cebca60257320f867bf8c586af488171d62
Jonathan Wolfe <jonathan.wolfe@noaa.gov>
2023-01-20 14:14:15 -0600

    vite with chartjs and ol

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import XYZ from 'ol/source/XYZ.js';
 import Stamen from 'ol/source/Stamen.js';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
+import Chart from 'chart.js/auto';
 
 
 const map = new Map({
@@ -31,7 +33,47 @@ const map = new Map({
     // })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([-90, 45]),
+    zoom: 8
   })
 });
+
+
+const ctx = document.getElementById("myChart");
+console.log(ctx);
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+          responsive:false,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
